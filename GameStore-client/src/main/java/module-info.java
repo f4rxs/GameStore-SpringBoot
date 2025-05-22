04328f1c257bd9b70dbc8fedb9487b0c45f5b61b
f4rxs <118629992+f4rxs@users.noreply.github.com>
2025-05-22 11:40:30 +0300

    Restructure repository to include client project

---
+++ GameStore-client/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.gamestoreclient {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.gamestoreclient to javafx.fxml;
+    exports com.example.gamestoreclient;
+}
